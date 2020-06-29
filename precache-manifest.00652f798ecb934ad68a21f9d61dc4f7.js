f513ae7aef27d2eb24e2ae70b41793829db5e1a9
manoaravindhan <man.oz@live.in>
2020-06-29 19:13:57 +0530

    Updates

---
+++ precache-manifest.00652f798ecb934ad68a21f9d61dc4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854986ac8a49f8be832c4bb1f6ff3828",
+    "url": "/agri-x/index.html"
+  },
+  {
+    "revision": "32563fd5085eb08c1357",
+    "url": "/agri-x/static/css/2.72f1c2a3.chunk.css"
+  },
+  {
+    "revision": "7aad255f9add6197a5be",
+    "url": "/agri-x/static/css/main.ba3e35c2.chunk.css"
+  },
+  {
+    "revision": "32563fd5085eb08c1357",
+    "url": "/agri-x/static/js/2.3c399382.chunk.js"
+  },
+  {
+    "revision": "7aad255f9add6197a5be",
+    "url": "/agri-x/static/js/main.8673b829.chunk.js"
+  },
+  {
+    "revision": "cc77e0297d6b63c56bbb",
+    "url": "/agri-x/static/js/runtime-main.10bd3d98.js"
+  },
+  {
+    "revision": "523b36afd9ea5cad47c8436b57226069",
+    "url": "/agri-x/static/media/result.523b36af.pdf"
+  }
+]);
